a2ec48d6f91213a0e9557a04d871655a0f34b73c
Tomohiro <t.nishiyama.wm@gmail.com>
2021-09-07 12:53:32 +0900

    7-commit

--- medNLP.py
+++ medNLP.py
@@ -7,26 +7,32 @@ with codecs.open("MedTxt-CR-JA-training.xml", "r", "utf-8") as file:
 
 
 # %%
+
+frequent_tags= ['d', 'a', 'timex3', 't-test', 't-key', 't-val', 'm-key', 'm-val']
+tags_value = [int(i) for i in range(1, len(frequent_tags)+1)]
+dict_tags = dict(zip(frequent_tags, tags_value))
+
 def entities_from_xml(file_name):
     import codecs
     from bs4 import BeautifulSoup
     with codecs.open(file_name, "r", "utf-8") as file:
         soup = BeautifulSoup(file, "html.parser")
 
-    for elem_articles in soup.find_all("articles"):
+    for elem_articles in soup.find_all("articles"):#articles内のarticleを一つずつ取り出す
         entities = []
         articles = []
-        for elem in elem_articles.find_all('article'):
+        for elem in elem_articles.find_all('article'):#article内の要素を一つずつ取り出す
             entities_article = []
             text_list = []
             pos1 = 0
             pos2 = 0
-            for child in elem:
+            for child in elem:#取り出した要素に対して，一つずつ処理する
+                #（タグのないものについても要素として取得されるので，位置(pos)はずれない）
                 text = child.string
 
                 pos2 += len(child.string)
-                if child.name != None:
-                    entities_article.append({'name':child.string, 'span':[pos1, pos2], 'type':child.name})
+                if child.name in frequent_tags:#特定のタグについて，固有表現の表現形，位置，タグを取得
+                    entities_article.append({'name':child.string, 'span':[pos1, pos2], 'type_id':dict_tags[child.name]})
                 pos1 = pos2
                 text_list.append(text)
             articles.append("".join(text_list))
@@ -40,14 +46,14 @@ articles = entities_from_xml('MedTxt-CR-JA-training.xml')[0]
 entities = entities_from_xml('MedTxt-CR-JA-training.xml')[1]
 
 # %%
-articles[0]
+entities[0]
 # %%
-data = []
+dataset = []
 for i in range(len(articles)):
-    data.append({'text': articles[i],\
+    dataset.append({'text': articles[i],\
     'entities': entities[i]})
 # %%
-data[1]
+dataset[1]
 # %%
 # 8-3
 import itertools
@@ -256,7 +262,7 @@ class NER_tokenizer_BIO(BertJapaneseTokenizer):
         return entities
 
 # %%
-tokenizer = NER_tokenizer_BIO.from_pretrained(MODEL_NAME, num_entity_type=8)
+tokenizer = NER_tokenizer_BIO.from_pretrained(MODEL_NAME, num_entity_type=len(frequent_tags))
 
 # %%
 text = '昨日のみらい事務所との打ち合わせは順調だった。'
@@ -270,19 +276,98 @@ encoding = tokenizer.encode_plus_tagged(
 print(encoding)
 # %%
 encoding = tokenizer.encode_plus_tagged(
-    data[0]['text'], data[0]['entities'], max_length=20
+    dataset[0]['text'], dataset[0]['entities'], max_length=512
 )
 print(encoding)
+
 # %%
-all_types = []
-for i in range(len(data[0]['entities'])):
-    all_types.append(data[0]['entities'][i]['type'])
-all_types=set(all_types)
+# データセットの分割
+random.shuffle(dataset)
+n = len(dataset)
+n_train = int(n*0.6)
+n_val = int(n*0.2)
+dataset_train = dataset[:n_train]
+dataset_val = dataset[n_train:n_train+n_val]
+dataset_test = dataset[n_train+n_val:]
 # %%
-all_types
+# 8-15
+def create_dataset(tokenizer, dataset, max_length):
+    """
+    データセットをデータローダに入力できる形に整形。
+    """
+    dataset_for_loader = []
+    for sample in dataset:
+        text = sample['text']
+        entities = sample['entities']
+        encoding = tokenizer.encode_plus_tagged(
+            text, entities, max_length=max_length
+        )
+        encoding = { k: torch.tensor(v) for k, v in encoding.items() }
+        dataset_for_loader.append(encoding)
+    return dataset_for_loader
+
+# トークナイザのロード
+tokenizer = NER_tokenizer.from_pretrained(MODEL_NAME)
+
+# データセットの作成
+max_length = 128
+dataset_train_for_loader = create_dataset(
+    tokenizer, dataset_train, max_length
+)
+dataset_val_for_loader = create_dataset(
+    tokenizer, dataset_val, max_length
+)
+
+# データローダの作成
+dataloader_train = DataLoader(
+    dataset_train_for_loader, batch_size=32, shuffle=True
+)
+dataloader_val = DataLoader(dataset_val_for_loader, batch_size=256)
 # %%
-types_value = [int(i) for i in range(1, len(all_types)+1)]
+# 8-16
+# PyTorch Lightningのモデル
+class BertForTokenClassification_pl(pl.LightningModule):
+        
+    def __init__(self, model_name, num_labels, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        self.bert_tc = BertForTokenClassification.from_pretrained(
+            model_name,
+            num_labels=num_labels
+        )
+        
+    def training_step(self, batch, batch_idx):
+        output = self.bert_tc(**batch)
+        loss = output.loss
+        self.log('train_loss', loss)
+        return loss
+        
+    def validation_step(self, batch, batch_idx):
+        output = self.bert_tc(**batch)
+        val_loss = output.loss
+        self.log('val_loss', val_loss)
+        
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+
+checkpoint = pl.callbacks.ModelCheckpoint(
+    monitor='val_loss',
+    mode='min',
+    save_top_k=1,
+    save_weights_only=True,
+    dirpath='model/'
+)
+
+trainer = pl.Trainer(
+    gpus=1,
+    max_epochs=5,
+    callbacks=[checkpoint]
+)
+
+# ファインチューニング
+model = BertForTokenClassification_pl(
+    MODEL_NAME, num_labels=9, lr=1e-5
+)
+trainer.fit(model, dataloader_train, dataloader_val)
+best_model_path = checkpoint.best_model_path
 # %%
-types_value
-# %%
-transform_types = zip()
